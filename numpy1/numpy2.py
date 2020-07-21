d20f68e80fa4d1121532b37b9f496783d05c74a0
the-pandey-ji <sonu.pandey_cs18@gla.ac.in>
2020-07-21 21:25:40 +0530

    create matrix of ones , zeroes and some random values

--- numpy1/numpy2.py
+++ numpy1/numpy2.py
@@ -1,3 +1,15 @@
 import numpy as np
 arr=np.ones(5)
 print (arr)
+
+# print matrix of ones
+arr1=np.ones((10,10),dtype=int)
+print(arr1)
+
+#print matrix of zeroes
+arr2=np.zeros((3,3),dtype=bool)
+print(arr2)
+
+# print random values
+arr3=np.empty((3,4),dtype=int)
+print(arr3)
